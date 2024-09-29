fdd03b586fe8bb0abb1a57f181116f823b4552f5
Conor Knowles <knoconor@amazon.com>
2024-09-29 20:26:06 +0100

    Fix curl outputs

--- partition-and-deploy.sh
+++ partition-and-deploy.sh
@@ -4,8 +4,8 @@ curl https://raw.githubusercontent.com/conorhk/nixos/refs/heads/mainline/disko.n
 nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko /tmp/disko.nix --arg device '"/dev/sda"'
 
 nixos-generate-config --no-filesystems --root /mnt
-curl https://raw.githubusercontent.com/conorhk/nixos/refs/heads/mainline/configuration.nix -o /mnt/persist/nixos/
-curl https://raw.githubusercontent.com/conorhk/nixos/refs/heads/mainline/flake.nix -o /mnt/persist/nixos/
-curl https://raw.githubusercontent.com/conorhk/nixos/refs/heads/mainline/home.nix -o /mnt/persist/nixos/
-curl https://raw.githubusercontent.com/conorhk/nixos/refs/heads/mainline/disko.nix -o /mnt/persist/nixos/
+curl https://raw.githubusercontent.com/conorhk/nixos/refs/heads/mainline/configuration.nix -o /mnt/persist/nixos/configuration.nix
+curl https://raw.githubusercontent.com/conorhk/nixos/refs/heads/mainline/flake.nix -o /mnt/persist/nixos/flake.nix
+curl https://raw.githubusercontent.com/conorhk/nixos/refs/heads/mainline/home.nix -o /mnt/persist/nixos/home.nix
+curl https://raw.githubusercontent.com/conorhk/nixos/refs/heads/mainline/disko.nix -o /mnt/persist/nixos/disko.nix
 nixos-install --root /mnt --flake /mnt/etc/nixos#default
